58f49512deb9ec1bc64d9fd7c65656586067153e
BagusSemesta2407 <semestabagus24@gmail.com>
2022-10-20 16:55:32 +0700

    update routes

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,4 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
-route::resource('project', projectController::class);
+route::resource('/', projectController::class);
