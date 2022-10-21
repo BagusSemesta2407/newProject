77c908c29cf3e7558ed0979e0b82acf9ce506bac
Lana Ismail <ismaillana22@gmail.com>
2022-10-21 11:24:55 +0700

    form tambah kategori

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,14 +2,42 @@
 
 @section('content')
 
-    <form action="">
-        <div class="mb-3">
-            <label for="name" class="form-label">Name</label>
-            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
-          </div>
-          <div class="mb-3">
-            <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
-            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-          </div>
-    </form>
+<div class="card mt-4">
+  <div class="card-header">
+    <h2>Kategori Project</h2>
+  </div>
+  <div class="card-body">
+      <div class="d-flex justify-content-between w-100">
+        <h4>Tambah Kategori Project</h4>
+        <a href="{{ route('project.create') }}"
+            class="btn btn-outline-success btn-lg d-flex align-items-center ">
+            <i class="fa fa-plus pr-2"></i>
+            Tambah
+        </a>
+      </div>
+      <div class="table-responsive">
+        <table class="table table-striped table-sm">
+          <thead>
+            <tr>
+              <th scope="col">No</th>
+              <th scope="col">Kategori</th>
+              <th scope="col">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1,001</td>
+              <td>random</td>
+              <td>data</td>
+            </tr>
+            <tr>
+              <td>1,002</td>
+              <td>placeholder</td>
+              <td>irrelevant</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+  </div>
+</div>
 @endsection
