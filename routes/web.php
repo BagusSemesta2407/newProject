75488037f103bf031317c924cfb59fa2fa5013d3
Lana Ismail <ismaillana22@gmail.com>
2022-10-21 10:17:41 +0700

    view dashboard

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,10 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
-Route::get('',function(){
-    return view('dashboard');
+Route::get('/dashboard',function(){
+    return view('Admin.dashboardAdmin');
+});
+Route::get('/',function(){
+    return view('index');
 });
 route::resource('/project', projectController::class)->middleware('auth');
